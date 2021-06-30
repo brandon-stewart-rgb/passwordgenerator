2f8341f3f715430b40337db0de1412a702cccbba
brandon-stewart-rgb <brandon@pixeltree.us>
2021-06-30 10:04:17 -0600

    store user confirmations to object

--- script.js
+++ script.js
@@ -3,33 +3,95 @@
 // User input variables: 
 var userInput;
 var confirmNumber;
-var confirmCharacter;
+var confirmSymbol;
 var confirmUppercase;
 var confirmLowercase;
 var generatePassword;
 
+// Assignment code here
 
-var password = writePassword();
-var passwordText = document.querySelector("#password");
-
-passwordText.value = password;
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
- // // Add event listener to generate button
-
- generateBtn.addEventListener("click", function () {
-  ps = generatePassword();
-  document.getElementById("password").placeholder = ps;
-});
+// password array
+let passwordArray = [];
 
 
+ 
+//puts each one of the following functions into object called randomFunction
 const randomFunction = {
   lower: randomLower,
   upper: randomUpper,
   number: randomNum,
   symbol: randomSymbol
 };
+
+ // Get references to the #generate element
+ var generateBtn = document.querySelector("#generate");
+
+
+
+ generateBtn.addEventListener('click', () => {
+	const length = +userInput;
+
+	// const hasLower = lowercaseEl.checked;
+	// const hasUpper = uppercaseEl.checked;
+	// const hasNumber = numbersEl.checked;
+	// const hasSymbol = symbolsEl.checked;
+	
+	passwordArray.innerText = writePassword();
+  
+});
+
+console.log(userInput);
+//    NEED TO GET PASSWORD LENGTH
+//    NEED TO WRITE PASSWORD TO INDEX.HTML
+
+
+// // Write password to the #password input
+function writePassword(lower, upper, number, symbol) {
+  // Ask user for length of pw
+  userInput = parseInt(prompt("How long would you like your password to be? Please choose between 8 and 128."));
+  
+  let generatedPassword = '';
+  console.log(generatePassword);
+// UNDEFINED
+
+  // If left blank
+  if (!userInput) {
+    alert("Please enter a number");
+
+  } else if (userInput < 8 || userInput > 128) {
+    userInput = parseInt(prompt("You must enter a number between 8 and 129"));
+
+  } else {
+    // Continues once user input is validated
+    confirmNumber = confirm("Would you like numbers?");
+    confirmSymbol= confirm("Would you like special symbols?");
+    confirmUppercase = confirm("Would you like Uppercase letters?");
+    confirmLowercase = confirm("Would you like Lowercase letters?"); 
+  };
+
+  if (!confirmNumber && !confirmUppercase && !confirmLowercase && !confirmSymbol) {
+    alert("Please choose at least one!");
+  }
+
+  const typesCount = confirmNumber + confirmUppercase + confirmLowercase + confirmSymbol;
+	const typesArr = [{ confirmNumber }, { confirmUppercase }, { confirmLowercase }, { confirmSymbol }].filter(item => Object.values(item)[0]);
+	console.log('typesArr:', typesArr);
+	// Doesn't have a selected type and is filtered out.
+	if(typesCount === 0) {
+		return '';
+	}
+
+  // create a loop
+	for(let i=0; i< userInput; i+=typesCount) {
+		typesArr.forEach(type => {
+			const funcName = Object.keys(type)[0];
+			generatedPassword += randomFunction[funcName]();
+		});
+	}
+
+    
+
+ };
 
 
 //Generator functions using browser character chart found here: https://net-comber.com/charset.html
@@ -57,34 +119,8 @@ function randomSymbol() {
 
 
 
-
-
-
-
-
-// // Write password to the #password input
-function writePassword() {
-  // Ask user for length of pw
-  userInput = parseInt(prompt("How long would you like your password to be? Please choose between 8 and 128."));
   
-  // If left blank
-  if (!userInput) {
-    alert("Please enter a number");
-  } else if (userInput < 8 || userInput > 128) {
-    userInput = parseInt(prompt("You must enter a number between 8 and 129"));
-  } else {
-    // Continues once user input is validated
-    confirmNumber = confirm("Would you like numbers?");
-    confirmCharacter = confirm("Would you like characters?");
-    confirmUppercase = confirm("Would you like Uppercase letters?");
-    confirmLowercase = confirm("Would you like Lowercase letters?");
-    
-  }
-
-
- };
 
  
-
 
 
