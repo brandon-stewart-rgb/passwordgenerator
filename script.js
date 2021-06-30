02148eb570ee382666acef5f8a1ecf326e05c62b
brandon-stewart-rgb <brandon@pixeltree.us>
2021-06-29 19:48:30 -0600

    add event listener

--- script.js
+++ script.js
@@ -1,73 +1,90 @@
 // Assignment code here
 
+// User input variables: 
+var userInput;
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase;
+var generatePassword;
 
 
+var password = writePassword();
+var passwordText = document.querySelector("#password");
 
-//Generator functions using browser character chart found here: https://net-comber.com/charset.html
-
-function getRandomLower() {
-  // random lowercase starts at 97
-  return String.fromCharCode(Math.floor(Math.random() *26) +97);
-};
-console.log(getRandomLower());
-
-
-
-function getRandomUpper() {
-  // random  uppercase starts at 65
-  return String.fromCharCode(Math.floor(Math.random() *26) +65);
-};
-console.log(getRandomUpper());
-
-
-
-function getRandomNum() {
-  // random  number starts at 48
-  return String.fromCharCode(Math.floor(Math.random() *10) +48);
-};
-console.log(getRandomNum());
-
-
-
-function getRandomSymbol() {
-  // random  number starts at 33
-  return String.fromCharCode(Math.floor(Math.random() *15) +33);
-};
-console.log(getRandomSymbol());
-
-
-
-
-
-
+passwordText.value = password;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+ // // Add event listener to generate button
+
+ generateBtn.addEventListener("click", function () {
+  ps = generatePassword();
+  document.getElementById("password").placeholder = ps;
+});
+
+
+const randomFunction = {
+  lower: randomLower,
+  upper: randomUpper,
+  number: randomNum,
+  symbol: randomSymbol
+};
+
+
+//Generator functions using browser character chart found here: https://net-comber.com/charset.html
+function randomLower() {
+  // random lowercase starts at 97
+  return String.fromCharCode(Math.floor(Math.random() *26) +97);
+};
+
+function randomUpper() {
+  // random  uppercase starts at 65
+  return String.fromCharCode(Math.floor(Math.random() *26) +65);
+};
+
+function randomNum() {
+  // random  number starts at 48
+  return String.fromCharCode(Math.floor(Math.random() *10) +48);
+};
+
+function randomSymbol() {
+  // random  number starts at 33
+  return String.fromCharCode(Math.floor(Math.random() *15) +33);
+};
+
+
+
+
+
+
+
+
+
 
 // // Write password to the #password input
 function writePassword() {
   // Ask user for length of pw
-  enter = parseInt(prompt("How long would you like your password to be?\nPlease choose between 8 and 128."));
-  console.log(enter);
+  userInput = parseInt(prompt("How long would you like your password to be? Please choose between 8 and 128."));
+  
   // If left blank
-  if (!enter) {
+  if (!userInput) {
     alert("Please enter a number");
-  } else if (enter < 8 || enter > 128) {
-
-    enter = parseInt(prompt("You must enter a number between 8 and 129"));
+  } else if (userInput < 8 || userInput > 128) {
+    userInput = parseInt(prompt("You must enter a number between 8 and 129"));
+  } else {
+    // Continues once user input is validated
+    confirmNumber = confirm("Would you like numbers?");
+    confirmCharacter = confirm("Would you like characters?");
+    confirmUppercase = confirm("Would you like Uppercase letters?");
+    confirmLowercase = confirm("Would you like Lowercase letters?");
+    
   }
 
 
-
-
-
-
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
-
-  //passwordText.value = password;
-
  };
 
-// // Add event listener to generate button
- generateBtn.addEventListener("click", writePassword);
+ 
+
+
+
